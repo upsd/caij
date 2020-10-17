c9c45f78947082229b8e700d7d5eb04519db61fb
Sam Davies <6772729+upsd@users.noreply.github.com>
2020-10-17 16:55:45 +0100

    Add testing for System.err.
    Introduced this to "ExpressionVerifier" as this makes things much easier for each test.

--- src/test/java/com/caij/lox/ParserWill.java
+++ src/test/java/com/caij/lox/ParserWill.java
@@ -2,6 +2,9 @@ package com.caij.lox;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -70,21 +73,31 @@ public class ParserWill {
     }
 
     @Test
-    public void return_null_if_parse_error_encountered() {
-        new ExpressionVerifier(eofOn(1)).returnsNull();
+    public void return_null_and_print_error_if_parse_error_encountered() {
+        new ExpressionVerifier(eofOn(1))
+                .returnsNull()
+                .reportedTheFollowingError("[line 1] Error at end: Expect expression.\n");
+
         new ExpressionVerifier(
                 new Token(TokenType.LEFT_PAREN, "(", "(", 1),
                 new Token(TokenType.NUMBER, "1", "1", 2),
-                eofOn(2)
-        ).returnsNull();
+                eofOn(3))
+                .returnsNull()
+                .reportedTheFollowingError("[line 3] Error at end: Expect ')' after expression.\n");
     }
 
     private class ExpressionVerifier {
 
         private final Token[] tokens;
+        private final ByteArrayOutputStream redirectedConsoleOutput;
+        private final PrintStream oldConsole;
 
         ExpressionVerifier(Token... tokens) {
             this.tokens = tokens;
+
+            redirectedConsoleOutput = new ByteArrayOutputStream();
+            oldConsole = System.out;
+            redirectSystemErrors();
         }
 
         void producesExpression(String expectedExpression) {
@@ -94,8 +107,24 @@ public class ParserWill {
             assertThat(visited).isEqualTo(expectedExpression);
         }
 
-        void returnsNull() {
+        ExpressionVerifier returnsNull() {
             assertThat(new Parser(asList(tokens)).parse()).isNull();
+            return this;
+        }
+
+        void reportedTheFollowingError(String expectedConsoleOutput) {
+            assertThat(redirectedConsoleOutput.toString()).isEqualTo(expectedConsoleOutput);
+            resetBackToConsole();
+        }
+
+        private void redirectSystemErrors() {
+            PrintStream ps = new PrintStream(redirectedConsoleOutput);
+            System.setErr(ps);
+        }
+
+        private void resetBackToConsole() {
+            System.out.flush();
+            System.setErr(oldConsole);
         }
     }
 
