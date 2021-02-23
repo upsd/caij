ffa9cc1c79e3d31a6bac43fed9b4f3e2269473f7
Sam Davies <6772729+upsd@users.noreply.github.com>
2021-02-23 23:18:43 +0000

    Introduce the notion of an enclosing environment.
    This will form the basis of there being a more global environment in which this environment exists. Instead of a "Russian Doll", think of this as an "Environment Doll", or "Environmentception". Okay that's enough for one commit.

--- src/main/java/com/caij/lox/Environment.java
+++ src/main/java/com/caij/lox/Environment.java
@@ -4,12 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
+    final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
+
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
+
+        // if variable is not found in this environment, try the enclosing one instead
+        if (enclosing != null) return enclosing.get(name);
 
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
@@ -23,7 +35,13 @@ public class Environment {
             values.put(name.lexeme, value);
             return;
         }
-        
+
+        // if we can't find variable in this environment, try and assign to the enclosing one instead
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 }
