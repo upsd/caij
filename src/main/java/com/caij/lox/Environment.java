ba900147159f8563b78004d4deebf207466ef5b5
Sam Davies <6772729+upsd@users.noreply.github.com>
2021-02-24 00:02:04 +0000

    Document "Environment".

--- src/main/java/com/caij/lox/Environment.java
+++ src/main/java/com/caij/lox/Environment.java
@@ -3,18 +3,34 @@ package com.caij.lox;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents a particular environment within program, storing variables already defined
+ */
 public class Environment {
     final Environment enclosing;
     private final Map<String, Object> values = new HashMap<>();
 
+    /**
+     * Construct an environment
+     */
     Environment() {
         enclosing = null;
     }
 
+    /**
+     * Construct an environment with enclosing environment
+     * @param enclosing the enclosing environment
+     */
     Environment(Environment enclosing) {
         this.enclosing = enclosing;
     }
 
+    /**
+     * Attempts to retrieve a variable from this environment, or from an enclosing environment (if specified)
+     * @param name name of variable to try and retrieve
+     * @return value of variable (if found)
+     * @throws RuntimeError if variable cannot be located
+     */
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
@@ -26,10 +42,21 @@ public class Environment {
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
 
+    /**
+     * Defines a new variable in this environment
+     * @param name name of variable
+     * @param value value to initialise (can be null)
+     */
     void define(String name, Object value) {
         values.put(name, value);
     }
 
+    /**
+     * Attempts to assign a value to a variable of this or an enclosing environment
+     * @param name name of variable
+     * @param value value to assign
+     * @throws RuntimeError if variable cannot be located
+     */
     void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
