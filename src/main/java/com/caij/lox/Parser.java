27d8f3a1d49aba47ebcee38097f2b8db803f1177
Sam Davies <6772729+upsd@users.noreply.github.com>
2021-03-07 19:13:30 +0000

    Interpret an if-statement.

--- src/main/java/com/caij/lox/Parser.java
+++ src/main/java/com/caij/lox/Parser.java
@@ -64,10 +64,26 @@ public class Parser {
     }
 
     private Stmt statement() {
+        if (match(IF)) return ifStatement();
         if (match(PRINT)) return printStatement();
         if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
         return expressionStatement();
+    }
+
+    private Stmt ifStatement() {
+        consume(LEFT_PAREN, "Expect '(' after 'if'.");
+        final Expr condition = expression();
+        consume(RIGHT_PAREN, "Expect ')' after if condition.");
+
+        final Stmt thenBranch = statement();
+        // Might not find an "else" branch
+        Stmt elseBranch = null;
+        if (match(ELSE)) {
+            elseBranch = statement();
+        }
+
+        return new Stmt.If(condition, thenBranch, elseBranch);
     }
 
     private Stmt expressionStatement() {
